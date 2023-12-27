4b1e610d5f1f0d902a3ecb462432fe6f57b5d356
Alice Alessandrini <alice.alessandrini.12.12.1993@gmail.com>
2023-12-27 18:42:57 +0100

    si può fareeee

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
 import { ListGroup } from "react-bootstrap";
-// import CommentsList from "./CommentsList";
-// import AddComment from "./AddComment";
 import CommentList from "./CommentList";
 
 class CommentArea extends Component {
+  /* Inizializzazione  STATO INIZIALE
+in cui l'array di comments è vuoto,
+loading è un boolean 
+e error è null */
   state = {
     comments: [],
     loading: false,
     error: null,
   };
+  /* QUI SI FA LA FETCH
+  ossia, una volta che il DOM ha caricato i suoi componenti viene fatta la fetch (per questo è ASINCRONA).
+  Lo stato viene cambiato su TRUE
+  */
   componentDidMount = async () => {
     this.setState({ loading: true, error: null });
+    {
+      /*FETCH VERA E PROPRIA CHE PARTE DA QUI. In cui:
+       vengono collegati i commenti relativi ad un libro specifico ( ${this.props.bookId} )
+      SE vengono trovati vengono caricati, altrimenti stampa un messaggio di errore */
+    }
     try {
       const response = await fetch(`https://striveschool-api.herokuapp.com/api/comments/${this.props.bookId}`, {
         headers: {
@@ -20,38 +31,47 @@ class CommentArea extends Component {
         },
       });
       if (response.ok) {
-        console.log("Data:", data);
         const data = await response.json();
+        console.log("Data:", data);
         this.setState({ comments: data, loading: false });
       } else {
         console.log("Error loading comments");
       }
     } catch (error) {
-      console.log("comments fetch error");
+      console.error("Error fetching comments:", error);
+      this.setState({ error, loading: false });
     }
+    /* Avevo un errore "Error fetching data: Cannot access 'data' before initialization" 
+    In pratica stavo cercando di stampare i dati PRIMA di aver fatto il parse del corpo della risposta JSON.
+    E' bastato invertire delle righe nell'if del try*/
 
     // onDelete da definire
   };
+
+  // RENDERIZZIAMO GLI ELEMENTI
+  /*  */
   render() {
     const { loading, error, comments } = this.state;
 
     return (
       <>
-        {loading && <p>loading...</p>}
-        {error && <p>error fetching data</p>}
-        {/*  */}
-        {this.state.comments.length > 0 ? (
-          <ListGroup variant="flush" style={{ marginTop: "1rem", overflowY: "scroll" }}>
-            {/*QUESTO DEVE DIVENTARE COMMENT LIST*/}
-            <CommentList comments={comments} />
-            {/*FINE COMMENT LIST*/}
-          </ListGroup>
+        {error && <p>Error fetching data: {error.message}</p>}
+        {/* Se loading è true, viene visualizzato un messaggio di caricamento. Se error è presente, viene visualizzato un messaggio di errore. */}
+        {/* Se ci sono commenti, viene mostrato un elenco (ListGroup) di commenti utilizzando il componente CommentList. Altrimenti, viene mostrato un messaggio che indica l'assenza di recensioni. */}
+        {loading ? (
+          <p>Loading...</p>
+        ) : comments.length > 0 ? (
+          <div style={{ height: "200px", marginTop: "1rem", overflowY: "scroll" }}>
+            <ListGroup variant="flush" style={{}}>
+              <CommentList comments={comments} />
+            </ListGroup>
+          </div>
         ) : (
-          <p>No reviews for this title</p>
+          <p>No comments available</p>
         )}
 
         {/* QUI ADD COMMENT CON IL FORM */}
-        {/* Assicurati di includere il codice per aggiungere commenti qui */}
+        {/* Includere il codice per aggiungere commenti qui */}
 
         {/* FINE COMMENT AREA */}
       </>
